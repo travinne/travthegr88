73da53cc3854c1781a43b1df829d05340d199a06
travinne <travinne.mutahi@moringaschool.com>
2025-03-27 09:03:14 +0300

    nearly done with the css

--- mkadi/test/game-stop/index.js
+++ mkadi/test/game-stop/index.js
@@ -1,90 +1,72 @@
-document.addEventListener('DOMContentLoaded',loadGames);
+let base_url =` http://localhost:3000/games`
 
-function selectPlan(plan){
-    alert('you selected this plan');
-}
-
-const games = [
-       {id:1,
-        name:'fifa 25',
-        Image:'https://pin.it/2I3Z2PV95',
-        description:'playstation 5',
-        price:'$ 65.oo'
-      },
-      {id:2,
-        name:'GTA V',
-        Image:'',
-        description:'playstation 4',
-        price:'$ 35.00'
-      },
-      {id:3,
-        name:'red ded redemtion',
-        Image:'',
-        description:'playstation 4',
-        price:'$57.00'
-      },
-      {id:4,
-        name:'assassins creed mirage',
-        Image:'',
-        description:'playstation 5',
-        price:'$ 55.00'
-      },
-      {id:5,
-        name:'need for speed unbound',
-        Image:'',
-        description:'playstation 5',
-        price:'$ 60.00 '
-      },
-      {id:6,
-        name:'astros play room',
-        Image:'',
-        description:'playstation 5',
-        price:'free'
-      },
-      {id:7,
-        name:'red ded redemtion 2',
-        Image:'',
-        description:'playstation 5',
-        price:'$ 65.00'
-      },
-      {id:8,
-        name:'call of duty',
-        Image:'',
-        description:'playstation 5',
-        price:'$ 75.00'
-      },
-      {id:9,
-        name:'GTA IV',
-        Image:'',
-        description:'playstation 5',
-        price:'$ 120.00'
-      },
-      {id:10,
-        name:'assassins creed shadows',
-        Image:'',
-        description:'playstation 5,x-box 1',
-        price:'$ 95.00',
-      }
-]
-
-function displayGames(){
-    const gameMenu = document.getElementById('container');
-    gameMenu.innerHTML = ""
-
-    games.forEach(game =>{
-        const img = document.createElement('img');
-        img.src = game.Image;
-        img.alt = game.name;
-        img.addEventListener('click',() => handleclick(game));
-        gameMenu.appendChild(img)
+function displayGames(games){
+    let gameLibrary = document.getElementById('gamelibrary')
+    gameLibrary.innerHTML = ""
+    games.forEach(game =>{  let html = `
+        <div id="single-game">
+            <h6>${game.title}</h6>
+            <img src="${game.image}" alt="${game.title}">
+            <div>${game.description}</div>
+            <div>${game.price}</div>
+            <div>
+            <button class="buy">buy<button>
+            </div>
+        </div>   
+        `
+        gameLibrary.innerHTML += html
     })
 }
 
-function handleclick(game){ 
-    document.getElementById('game.name').textContent = game.name;
-    document.getElementById('game.image').src = game.Image;
-    document.getElementById('game.description').textContent = game.description;
-    document.getElementById('game.price').textContent = game.price;
+fetch(base_url)
+.then(res =>res.json())
+.then(data => {
+     console.log(data);
+     displayGames(data);})
+.catch(err => console.log( "error occured in fetching game",err))
+
+
+document.getElementById('gameForm').addEventListener('submit',function(event){
+    event.preventDefault();
+    let gameTitle = document.getElementById('title').value;
+    let gameImage = document.getElementById('image').value;
+    let gameDescription = document.getElementById('description').value;
+    let gamePrice = parseFloat(document.getElementById('title').value);
+
+    let gameObject = {
+        title: gameTitle,
+        image: gameImage,
+        description: gameDescription,
+        price: gamePrice
+    };
+
+fetch(base_url,{
+    method: "POST",
+    headers: {"Content-Type" : "application/json"},
+    body: JSON.stringify(gameObject
+    )
+})
+    .then(res => res.json())
+    .then(data => {console.log("game added",data);
+     fetch(base_url)
+    .then(res => res.json())
+    .then(updatedData => displayGames(updatedData));
+})
+    .catch(err => console.log("error occured while adding game",err));
+})
+
+function deleteGame(id){
+    fetch(`${base_url}/${id}`,{
+        method: "DELETE"})
+        .then(res => res.json)
+        .then(() => {console.log(`you have successfuly deleted ${id}`)
+      fetch(base_url)
+    .then(res => res.json())
+    .then(updatedData => displayGames(updatedData));
+})
+        .catch(err => console.log(err));
 }
 
 
+        
+        
